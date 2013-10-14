c349890f2289667f6927e731f4a987ac2725c42f
王凯 <kai.wang7@renren-inc.com>
2013-10-14 13:09:20 +0800

    Revert "add codes to trigger the master's view appear and disapper related methods."
    This reverts commit 5835522b3b318834f53bda4483b0ebc31876583b.

--- XLSplitViewControllerSample/XLSplitViewControllerSample/CustomSplitViewController.m
+++ XLSplitViewControllerSample/XLSplitViewControllerSample/CustomSplitViewController.m
@@ -20,21 +20,20 @@
 /*
     if you use storyboard, you could use like this way. just initialize the master and detail viewControllers.
  */
-- (void)viewDidLoad
+- (void)awakeFromNib
 {
-    [super viewDidLoad];
     // first initialize the two viewControllers.
     UINavigationController *masterNavController = [self.storyboard instantiateViewControllerWithIdentifier:@"MasterNavigationController"];
     UINavigationController *detailNavController = [self.storyboard instantiateViewControllerWithIdentifier:@"DetailNavigationController"];
-
+    
     /*  second, set the splitViewController to each viewController.
-     note that this step is not necessary. this is the convenient way to make the master and detail know each other.
-     apple makes a readOnly property 'splitViewController' for UIViewController to find the container splitViewController,
-     but I cannot use this property. So, I have to do things this way.
+        note that this step is not necessary. this is the convenient way to make the master and detail know each other.
+        apple makes a readOnly property 'splitViewController' for UIViewController to find the container splitViewController,
+        but I cannot use this property. So, I have to do things this way.
      */
     MasterViewController *masterViewController = (MasterViewController *)masterNavController.topViewController;
     masterViewController.xlSplitViewController = self;
-
+    
     DetailViewController *detailViewController = (DetailViewController *)detailNavController.topViewController;
     detailViewController.xlSplitViewController = self;
 
@@ -43,7 +42,7 @@
 
     //customize
     self.masterWidth = 260;
-
+    
     //setup the two viewControllers
     self.viewControllers = @[masterNavController, detailNavController];
 }
